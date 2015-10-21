3016c06837e6335909aadf24121800d5f7fa7330
Hugh Sorby <h.sorby@auckland.ac.nz>
2015-10-22 10:57:32 +1300

    Update version to 0.3.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ ready for consumption by other libraries.
 """
 
 setup(name=u'meshparser',
-      version='0.3.0',
+      version='0.3.1',
       description='A Small Python library for parsing files that describe a mesh.',
       long_description=long_description,
       classifiers=[],

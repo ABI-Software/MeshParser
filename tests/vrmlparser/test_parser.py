add7e57cdd8fc885a74c6d8caa925c3eb20d28e0
Hugh Sorby <h.sorby@auckland.ac.nz>
2015-10-21 22:44:57 +1300

    Integrate the point paring into the getElements and getPoints of the STLParser code.  Also added zero offset option for element point indexes.

--- tests/vrmlparser/test_parser.py
+++ tests/vrmlparser/test_parser.py
@@ -10,28 +10,25 @@ from meshparser.nodepare.pare import NodePare
 
 
 class ParserTestCase(unittest.TestCase):
-
-
     def testExistence(self):
         v = VRMLParser()
-        self.assertRaises(FileNotFoundError, v.parse, 'file that doesnt exist')
-        
+        self.assertRaises(IOError, v.parse, 'file that doesnt exist')
+
     def testParse1(self):
         v = VRMLParser()
-        v.parse('data/Horse_1_1.wrl') 
+        v.parse('data/Horse_1_1.wrl')
 
     def testParse2(self):
         v = VRMLParser()
         v.parse('data/Horse_1_2.wrl')
-        
 
     def testParse3(self):
         v = VRMLParser()
         v.parse('data/Horse_1_3.wrl')
-        
+
         self.assertEqual(4572, len(v.getPoints()))
-        self.assertEqual(36480, len(v.getElements()))
-        
+        self.assertEqual(9120, len(v.getElements()))
+
     def testDuplicatedPoints(self):
         v = VRMLParser()
         v.parse('data/Horse_1_4.wrl')
@@ -40,9 +37,8 @@ class ParserTestCase(unittest.TestCase):
         np.addPoints(v.getPoints())
         np.parePoints()
         self.assertEqual(23, len(np.getParedPoints()))
-        
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
